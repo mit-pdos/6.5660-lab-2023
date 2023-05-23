4326b641a6857b36cff86333a3a080bd45e54620
Ariel Szekely <arielck@mit.edu>
2023-05-23 11:43:31 -0400

    Add some more try-catches

--- lab5-tests/grade-webauthn.mjs
+++ lab5-tests/grade-webauthn.mjs
@@ -23,131 +23,167 @@ async function main() {
     var grader2_cred = null;
     var opts = {};
 
-    await grading.zoobarRegister(zoobar_real, "grader1", async function(page, ok) {
-        if (ok) {
-            grader1_cred = await saveCred(page);
-            grading.passed("Registered grader1");
-        } else {
-            grading.failed("Registering grader1 failed");
-            phantom.exit();
-        }
-    });
+    try {
+      await grading.zoobarRegister(zoobar_real, "grader1", async function(page, ok) {
+          if (ok) {
+              grader1_cred = await saveCred(page);
+              grading.passed("EX1 Registered grader1");
+          } else {
+              grading.failed("EX1 Registering grader1 failed");
+              phantom.exit();
+          }
+      });
+    } catch (error) {
+      grading.failed("EX1 ERROR: " + error.message);
+    }
 
-    grader1_cred.rpId = zoobar_real;
-    await grading.zoobarLogin(zoobar_real, "grader1", grader1_cred, opts, async function(page, ok) {
-        if (ok) {
-            grading.passed("Logged in grader1");
-        } else {
-            grading.failed("Logging in as grader1 failed");
-            phantom.exit();
-        }
-    });
+    try {
+      grader1_cred.rpId = zoobar_real;
+      await grading.zoobarLogin(zoobar_real, "grader1", grader1_cred, opts, async function(page, ok) {
+          if (ok) {
+              grading.passed("EX2 Logged in grader1");
+          } else {
+              grading.failed("EX2 Logging in as grader1 failed");
+              phantom.exit();
+          }
+      });
+    } catch (error) {
+      grading.failed("EX2 ERROR: " + error.message);
+    }
 
-    opts.overrideCredsGetChallenge = opts.credsGetChallenge;
-    await grading.zoobarLogin(zoobar_real, "grader1", grader1_cred, opts, async function(page, ok) {
-        if (ok) {
-            grading.failed("Logged in grader1 with replayed challenge");
-            phantom.exit();
-        } else {
-            grading.passed("Login with replayed challenge blocked");
-        }
-    });
+    try {
+      opts.overrideCredsGetChallenge = opts.credsGetChallenge;
+      await grading.zoobarLogin(zoobar_real, "grader1", grader1_cred, opts, async function(page, ok) {
+          if (ok) {
+              grading.failed("EX3 Logged in grader1 with replayed challenge");
+              phantom.exit();
+          } else {
+              grading.passed("EX3 Login with replayed challenge blocked");
+          }
+      });
+    } catch (error) {
+      grading.failed("EX3 ERROR: " + error.message);
+    }
 
     try {
       opts.overrideCredsGetChallenge = null;
       grader1_cred.rpId = zoobar_fake;
       await grading.zoobarLogin(zoobar_fake, "grader1", grader1_cred, opts, async function(page, ok) {
           if (ok) {
-              grading.failed("Logged in as grader1 from wrong origin");
+              grading.failed("EX4 Logged in as grader1 from wrong origin");
               phantom.exit();
           } else {
-              grading.passed("Login from wrong origin blocked");
+              grading.passed("EX4 Login from wrong origin blocked");
           }
       });
     } catch (error) {
-      grading.failed("Logged in as grader1 from wrong origin\nError: " + error.message);
+      grading.failed("EX4 ERROR: " + error.message);
     }
 
-    await grading.zoobarRegister(zoobar_real, "grader2", async function(page, ok) {
-        if (ok) {
-            grader2_cred = await saveCred(page);
-            grading.passed("Registered grader2");
-        } else {
-            grading.failed("Registering grader2 failed");
-            phantom.exit();
-        }
-    });
+    try {
+      await grading.zoobarRegister(zoobar_real, "grader2", async function(page, ok) {
+          if (ok) {
+              grader2_cred = await saveCred(page);
+              grading.passed("EX5-1 Registered grader2");
+          } else {
+              grading.failed("EX5-1 Registering grader2 failed");
+              phantom.exit();
+          }
+      });
 
-    grader2_cred.rpId = zoobar_real;
-    opts.credsGetCallback = async function() {
-        opts.credsGetCallback = null;
-        await grading.zoobarLogin(zoobar_real, "grader2", grader2_cred, opts, async function(page, ok) {
-            if (ok) {
-                grading.passed("Logged in grader2 concurrently (inner)");
-            } else {
-                grading.failed("Concurrent login as grader2 failed (inner)");
-                phantom.exit();
-            }
-        });
-    };
+      grader2_cred.rpId = zoobar_real;
+      opts.credsGetCallback = async function() {
+          opts.credsGetCallback = null;
+          await grading.zoobarLogin(zoobar_real, "grader2", grader2_cred, opts, async function(page, ok) {
+              if (ok) {
+                  grading.passed("EX5-2 Logged in grader2 concurrently (inner)");
+              } else {
+                  grading.failed("EX5-2 Concurrent login as grader2 failed (inner)");
+                  phantom.exit();
+              }
+          });
+      };
 
-    await grading.zoobarLogin(zoobar_real, "grader2", grader2_cred, opts, async function(page, ok) {
-        if (ok) {
-            grading.passed("Logged in grader2 concurrently (outer)");
-        } else {
-            grading.failed("Concurrent login as grader2 failed (outer)");
-            phantom.exit();
-        }
-    });
+      await grading.zoobarLogin(zoobar_real, "grader2", grader2_cred, opts, async function(page, ok) {
+          if (ok) {
+              grading.passed("EX5-3 Logged in grader2 concurrently (outer)");
+          } else {
+              grading.failed("EX5-3 Concurrent login as grader2 failed (outer)");
+              phantom.exit();
+          }
+      });
+    } catch (error) {
+      grading.failed("EX5 ERROR: " + error.message);
+    }
 
-    opts.overrideCredsGetChallenge = [0];
-    await grading.zoobarLogin(zoobar_real, "grader2", grader2_cred, opts, async function(page, ok) {
-        if (ok) {
-            grading.failed("Logged in grader2 with bogus challenge");
-            phantom.exit();
-        } else {
-            grading.passed("Login as grader2 with bogus challenge blocked");
-        }
-    });
+    try {
+      opts.overrideCredsGetChallenge = [0];
+      await grading.zoobarLogin(zoobar_real, "grader2", grader2_cred, opts, async function(page, ok) {
+          if (ok) {
+              grading.failed("EX6 Logged in grader2 with bogus challenge");
+              phantom.exit();
+          } else {
+              grading.passed("EX6 Login as grader2 with bogus challenge blocked");
+          }
+      }); 
+    } catch (error) {
+      grading.failed("EX6 Logged in as grader1 from wrong origin ERROR: " + error.message);
+    }
 
-    opts.overrideCredsGetChallenge = opts.credsGetChallenge;
-    grader1_cred.rpId = zoobar_real;
-    await grading.zoobarLogin(zoobar_real, "grader1", grader1_cred, opts, async function(page, ok) {
-        if (ok) {
-            grading.failed("Logged in grader1 with challenge from grader2");
-            phantom.exit();
-        } else {
-            grading.passed("Login as grader1 with mismatched challenge blocked");
-        }
-    });
+    try {
+      opts.overrideCredsGetChallenge = opts.credsGetChallenge;
+      grader1_cred.rpId = zoobar_real;
+      await grading.zoobarLogin(zoobar_real, "grader1", grader1_cred, opts, async function(page, ok) {
+          if (ok) {
+              grading.failed("EX7 Logged in grader1 with challenge from grader2");
+              phantom.exit();
+          } else {
+              grading.passed("EX7 Login as grader1 with mismatched challenge blocked");
+          }
+      });
+    } catch (error) {
+      grading.failed("EX7 Logged in as grader1 from wrong origin ERROR: " + error.message);
+    }
 
-    grader2_cred.privateKey = grader1_cred.privateKey;
-    await grading.zoobarLogin(zoobar_real, "grader2", grader2_cred, opts, async function(page, ok) {
-        if (ok) {
-            grading.failed("Logged in as grader2 with wrong private key");
-            phantom.exit();
-        } else {
-            grading.passed("Login with wrong private key blocked");
-        }
-    });
+    try {
+      grader2_cred.privateKey = grader1_cred.privateKey;
+      await grading.zoobarLogin(zoobar_real, "grader2", grader2_cred, opts, async function(page, ok) {
+          if (ok) {
+              grading.failed("EX8 Logged in as grader2 with wrong private key");
+              phantom.exit();
+          } else {
+              grading.passed("EX8 Login with wrong private key blocked");
+          }
+      });
+    } catch (error) {
+      grading.failed("EX8 ERROR: " + error.message);
+    }
 
+    try {
     await grading.zoobarRegister(zoobar_real, "grader1", async function(page, ok) {
         if (ok) {
-            grading.failed("Registered twice as grader1");
+            grading.failed("EX9 Registered twice as grader1");
             phantom.exit();
         } else {
-            grading.passed("Duplicate registration for grader1 blocked");
+            grading.passed("EX9 Duplicate registration for grader1 blocked");
         }
     });
+    } catch (error) {
+      grading.failed("EX9 ERROR: " + error.message);
+    }
 
-    await grading.zoobarRegister(zoobar_fake, "grader3", async function(page, ok) {
-        if (ok) {
-            grading.failed("Registered from wrong origin");
-            phantom.exit();
-        } else {
-            grading.passed("Registration from wrong origin blocked");
-        }
-    });
+    try {
+      await grading.zoobarRegister(zoobar_fake, "grader3", async function(page, ok) {
+          if (ok) {
+              grading.failed("EXA Registered from wrong origin");
+              phantom.exit();
+          } else {
+              grading.passed("EXA Registration from wrong origin blocked");
+          }
+      });
+    } catch (error) {
+      grading.failed("EXA ERROR: " + error.message);
+    }
 
     phantom.exit();
 }
